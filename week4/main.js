7cdfc49f030aacd2b4b2243d03d9e54106a463e5
HudaNayyer <114230183+HudaNayyer@users.noreply.github.com>
2023-11-03 10:08:10 +0400

    json link added

--- week4/main.js
+++ week4/main.js
@@ -1,6 +1,10 @@
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://f28wp-lab1/week4/cities1.json');
+ourRequest.open('GET', 'https://hudanayyer.github.io/F28WP-lab1/week4/cities1.json');
 ourRequest.onload = function() {
-console.log(ourRequest.responseText);
+var ourData = JSON.parse(ourRequest.responseText);
+console.log(ourData[0]);
 };
 ourRequest.send();
+})
