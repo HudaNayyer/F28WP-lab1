90c9e148e37ce6619133200f4c3b55c5c70aeb4e
HudaNayyer <114230183+HudaNayyer@users.noreply.github.com>
2023-11-03 09:58:34 +0400

    json

--- week4/main.js
+++ week4/main.js
@@ -1,5 +1,5 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
+ourRequest.open('GET', 'https://f28wp-lab1/week4/cities1.json');
 ourRequest.onload = function() {
 console.log(ourRequest.responseText);
 };
