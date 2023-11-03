098f260ec09303b2cecd1a0872678de2e79ee4ca
HudaNayyer <114230183+HudaNayyer@users.noreply.github.com>
2023-11-03 09:50:11 +0400

    json file

---
+++ week4/js.js
@@ -0,0 +1,32 @@
+var myCity = {
+    "name": "Dubai",
+    "country":"UAE",
+    "place":"Dubai Mall"
+}
+console.log (myCity.nameame);
+
+
+console.log(myCity.countryountry);
+
+var myCity = ["Dubai", "UAE", "Dubai Mall"];
+console.log(myCity[0]); 
+
+var myCities = [
+    {
+    "name": "Dubai",
+    "country": "UAE",
+    "place": "Dubai Mall"
+    },
+    {
+    "name": "Milan",
+    "country": "Italy",
+    "place": "Duomo Di Milano"
+    },
+    {
+        "name" : "Abu Dhabi",
+        "country":"UAE",
+        "place":"Marina Mall"
+    }
+    ];
+
+    console.log(myCities[1].country); // Outputs: Italy
